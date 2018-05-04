d93a5f094a2f2526103d54ac00b0ef264bf71e4c
Tim Taber <developer@timothytaber.com>
2018-05-03 19:58:41 -0800

    Test to combine OLED with temperature sensors

--- kegerator.py
+++ kegerator.py
@@ -1,43 +1,23 @@
 # Kegerator
 # 2018.05.03
 
-import Adafruit_DHT
-
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-sensor = Adafruit_DHT.AM2302
-
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
-#pin = 'P8_11'
-
-# Example using a Raspberry Pi with DHT sensor
-# connected to GPIO23.
-pin = 24
-
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-
 import time
 
-import Adafruit_GPIO.SPI as SPI
+import Adafruit_DHT
 import Adafruit_SSD1306
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import subprocess
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.AM2302
+
+towerPin = 23
+basePin = 24
+towerTemp = 0
+baseTemp = 0
 
 # Initialize 128x64 OLED display with hardware I2C:
 RST = None
@@ -76,28 +56,29 @@ font = ImageFont.load_default()
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 while True:
+    checkTemps()
+    refreshScreen()
+
+def refreshScreen():
 
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
-
     # Write two lines of text.
 
-    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+    draw.text((x, top),       "Base={0:0.1f}F".format(baseTemp), font=font, fill=255)
+    draw.text((x, top+8),     "Tower={0:0.1f}F".format(towerTemp), font=font, fill=255)
 
     # Display image.
     disp.image(image)
     disp.display()
     time.sleep(.1)
+
+def checkTemps():
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    towerHum, towerCels = Adafruit_DHT.read_retry(sensor, towerPin)
+    baseHum, baseCels = Adafruit_DHT.read_retry(sensor, basePin)
+
+    global towerTemp = towerCels * 1.8 + 32
+    global baseTemp = baseCels * 1.8 + 32
